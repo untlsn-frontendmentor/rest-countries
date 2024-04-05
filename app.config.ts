23c8d970c6a441f5a9e9c22d92c1cb7664911987
Filip Skoczen <philipskoczen@gmail.com>
2024-04-05 21:43:41 +0200

    Add preset netlify

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "@solidjs/start/config";
+import { defineConfig } from '@solidjs/start/config';
 import unocss from 'unocss/vite';
 
 export default defineConfig({
+	server: {
+		preset: 'netlify',
+	},
 	vite: {
 		plugins: [
 			unocss(),
-		]
-	}
+		],
+	},
 });
